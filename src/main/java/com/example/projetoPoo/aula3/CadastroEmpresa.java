3e4613f8c7a57ddb84a7fad64a6ea6c4768576bc
flaviospjunior <33583206+flaviospjunior@users.noreply.github.com>
2021-03-23 00:25:44 -0300

    Delete src/main/java/com/example/projetoPoo/aula3 directory

--- src/main/java/com/example/projetoPoo/aula3/CadastroEmpresa.java
+++
@@ -1,7 +0,0 @@
-package com.example.projetoPoo.aula3;
-
-public class CadastroEmpresa extends CadastrosGerais {
-    public CadastroEmpresa(String nome) {
-        super(nome);
-    }
-}
--- src/main/java/com/example/projetoPoo/aula3/Principal.java
+++
@@ -1,7 +0,0 @@
-package com.example.projetoPoo.aula3;
-
-public class Principal {
-    public static void main(String[] args) {
-
-    }
-}
