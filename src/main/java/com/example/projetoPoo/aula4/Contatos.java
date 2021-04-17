56a23fe01a711dcc1e51803472209e3d19a5d887
flaviospjunior <flaviospjunior@gmail.com>
2021-04-17 19:03:27 -0300

    Aula4

--- src/main/java/com/example/projetoPoo/aula4/Contatos.java
+++ src/main/java/com/example/projetoPoo/aula4/Contatos.java
@@ -1,5 +1,20 @@
 package com.example.projetoPoo.aula4;
 
-public class Contatos {
+import java.util.Scanner;
 
+public class Contatos {
+    Scanner scanner = new Scanner(System.in);
+
+    public int getOpcaoOperacao() {
+        return opcaoOperacao;
+    }
+
+    private int opcaoOperacao = scanner.nextInt();
+    String contato = AdicionarContato().toString();
+    String numeroTelefone;
+
+   public CharSequence AdicionarContato(){
+       CharSequence contatoDigitado = scanner.nextLine();
+       return contatoDigitado;
+    }
 }
--- src/main/java/com/example/projetoPoo/aula4/Principal.java
+++ src/main/java/com/example/projetoPoo/aula4/Principal.java
@@ -3,8 +3,12 @@ package com.example.projetoPoo.aula4;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 
 public class Principal {
 
@@ -15,12 +19,13 @@ public class Principal {
         Path diretorioArquivoContatos = Path.of("src", "main", "resources", "aula4", "Contatos");  //Função responsável por criar o diretório na variável diretorioArquivoContatos
         System.out.println(diretorioArquivoContatos.toAbsolutePath());
 
+                List <String> listagemContatos = new ArrayList<>();
         try {
             if (Files.exists(diretorioArquivoContatos)) {
                 logger.info("Diretório já existe.");
             } else {
                 Files.createDirectories(diretorioArquivoContatos);
-                logger.info("Diretório criado com sucesso.");
+                logger.info("Diretório criado com sucesso!");
             }
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Não foi possível criar o diretório");
@@ -29,7 +34,7 @@ public class Principal {
         final Path listaContatos = diretorioArquivoContatos.resolve("AgendaDeContatos.txt");
         try {
             if (Files.exists((listaContatos))) {
-                logger.info("Arquivo não foi criado pois já existe");
+                logger.info("Arquivo não foi criado pois já existe!");
             } else {
                 Files.createFile(listaContatos);
                 logger.info("Arquivo criado com sucesso!");
@@ -37,6 +42,23 @@ public class Principal {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Contatos contatos = new Contatos();
+
+        if (contatos.getOpcaoOperacao()== 1){
+            System.out.println("Digite o contato:");
+
+            listagemContatos.add(contatos.contato);
+            try {
+                Files.write(listaContatos, listagemContatos);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
     }
 }
+
+
 
